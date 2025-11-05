b8ac42eb242249bc5afb1802c633d634174f73c9
GoldLion7288 <candoeverything728@gmail.com>
2025-11-05 22:37:22 +0900

    add: data

--- realrun.sh
+++ realrun.sh
@@ -1,5 +1,5 @@
-~/advertisement/run.sh start ~/data/background.jpg 1
-~/advertisement/run.sh play ~/data/test1.mp4 20
+~/advertisement/run.sh start ~/advertisement/data/background.jpg 1
+~/advertisement/run.sh play ~/advertisement/data/test1.mp4 20
 ~/advertisement/run.sh play ~/data/test2.mp4 20
 ~/advertisement/run.sh play ~/data/test3.mp4 10
 ~/advertisement/run.sh play ~/data/test4.mp4 20
