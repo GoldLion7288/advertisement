01bd7948b17cac9a526b46f7cc92cdd23f008b6b
GoldLion7288 <candoeverything728@gmail.com>
2025-11-12 08:40:02 +0900

    new step: more optimize ++

--- realrun.sh
+++ realrun.sh
@@ -1,8 +1,10 @@
-~/advertisement/run.sh start ~/advertisement/data/background.jpg 1
-~/advertisement/run.sh play ~/advertisement/data/test1.mp4 20
-~/advertisement/run.sh play ~/advertisement/data/test2.mp4 20
-~/advertisement/run.sh play ~/advertisement/data/test3.mp4 10
-~/advertisement/run.sh play ~/advertisement/data/test4.mp4 20
-~/advertisement/run.sh play ~/advertisement/data/test5.mp4 15
+~/advertisement/run.sh start ~/data/background.jpg 1
+~/advertisement/run.sh play ~/data/test1.mp4 20
+~/advertisement/run.sh play ~/data/test1.jpg 1
+~/advertisement/run.sh play ~/data/test2.mp4 20
+~/advertisement/run.sh play ~/data/test2.jpg 1
+~/advertisement/run.sh play ~/data/test3.mp4 10
+~/advertisement/run.sh play ~/data/test4.mp4 20
+~/advertisement/run.sh play ~/data/test5.mp4 15
 ~/advertisement/run.sh stop
 ~/advertisement/run.sh exit
