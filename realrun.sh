a44cfe22044339476591cc1f2255d9ebc596ed13
GoldLion7288 <candoeverything728@gmail.com>
2025-11-12 15:19:12 +0900

    new step: step2

--- realrun.sh
+++ realrun.sh
@@ -5,6 +5,6 @@
 ~/advertisement/run.sh play ~/data/test2.jpg 1
 ~/advertisement/run.sh play ~/data/test3.mp4 10
 ~/advertisement/run.sh play ~/data/test4.mp4 20
-~/advertisement/run.sh play ~/data/test5.mp4 15
+~/advertisement/run.sh play ~/data/test5.mp4 30
 ~/advertisement/run.sh stop
 ~/advertisement/run.sh exit
