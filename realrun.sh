089e83e949793a86de048177515b8ac31f5ab9e3
GoldLion7288 <candoeverything728@gmail.com>
2025-11-14 23:25:06 +0900

    fix: update realrun.sh+

--- realrun.sh
+++ realrun.sh
@@ -1,7 +1,7 @@
-~/advertisement/run.sh start ~/data/background.jpg 1
-~/advertisement/run.sh play ~/data/test1.mp4 20
-~/advertisement/run.sh play ~/data/test1.jpg 1
-~/advertisement/run.sh play ~/data/test2.jpg 1
-~/advertisement/run.sh play ~/data/test2.mp4 20
+~/advertisement/run.sh start ~/advertisement/data/background.jpg 1
+~/advertisement/run.sh play ~/advertisement/data/test1.mp4 20
+~/advertisement/run.sh play ~/advertisement/data/test1.jpg 1
+~/advertisement/run.sh play ~/advertisement/data/test2.jpg 1
+~/advertisement/run.sh play ~/advertisement/data/test2.mp4 20
 ~/advertisement/run.sh stop
 ~/advertisement/run.sh exit
